14214e687b5d3b4effc49264f4410a8d6e3baa38
AvniErtas <avniertas@outlook.com>
2020-04-09 22:59:50 +0300

    update

--- tanismauygulamasi/lib/Anasayfa/abcdTest.dart
+++ tanismauygulamasi/lib/Anasayfa/abcdTest.dart
@@ -1,4 +1,7 @@
-import'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:tanismauygulamasi/Sayfalar/gradientcard.dart';
+import 'package:tanismauygulamasi/Sayfalar/gradientcolor.dart';
 
 class AbcdTestBolumu extends StatefulWidget {
   @override
@@ -9,7 +12,134 @@ class _AbcdTestBolumuState extends State<AbcdTestBolumu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("S.A"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(gradient: GradientColors.arkaplan),
+              child: testevethayirBolumu(),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget testevethayirBolumu() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        buttonDemo(context),
+        Expanded(child: abcdSiklari()),
+      ],
+    );
+  }
+
+  Widget buttonDemo(context) {
+    final basicSlider = CarouselSlider.builder(
+      itemCount: 5,
+      itemBuilder: (BuildContext context, int itemIndex) => GradientCard(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        gradient: GradientColors.anasayfaswiper,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("asdf"),
+              ],
+            ),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 20, bottom: 20),
+                  child: Text("asdf"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          basicSlider,
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Flexible(
+              child: InkWell(
+                onTap: () {
+                  basicSlider.previousPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.linear);
+                },
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/digericonlar/geri.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: InkWell(
+                onTap: () {
+                  basicSlider.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.linear);
+                },
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/digericonlar/ileri.png"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ]);
+  }
+
+  Widget abcdSiklari() {
+    return ListView.builder(
+        itemCount: 4,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 75,
+            width: 100,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          );
+        });
   }
 }
--- tanismauygulamasi/lib/Sayfalar/arkadaslistesi.dart
+++ tanismauygulamasi/lib/Sayfalar/arkadaslistesi.dart
@@ -1,15 +1,79 @@
+// Flutter code sample for CheckboxListTile
+
+// ![CheckboxListTile sample](https://flutter.github.io/assets-for-api-docs/assets/material/checkbox_list_tile.png)
+//
+// This widget shows a checkbox that, when checked, slows down all animations
+// (including the animation of the checkbox itself getting checked!).
+//
+// This sample requires that you also import 'package:flutter/scheduler.dart',
+// so that you can reference [timeDilation].
+
 import 'package:flutter/material.dart';
 
-class ArkadasListesi extends StatefulWidget {
-  @override
-  _ArkadasListesiState createState() => _ArkadasListesiState();
-}
+import 'package:flutter/scheduler.dart' show timeDilation;
 
-class _ArkadasListesiState extends State<ArkadasListesi> {
+/// This Widget is the main application widget.
+class ArkadasListesi extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    ); 
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  List<bool> liste = new List<bool>();
+  @override
+  void initState() {
+    for (int i = 0; i < 10; i++) {
+      liste.add(false);
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.separated(
+        separatorBuilder: (contex, index) {
+          return Divider(height: 20,thickness: 2,color: Colors.blue.shade100,);
+        },
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return CheckboxListTile(
+            title: const Text('Kullanıcı Adı Gelecek'),
+            value: liste[index],
+            onChanged: (bool value) {
+              setState(() {
+                timeDilation = value ? 2.0 : 1.0;
+                liste[index] = value;
+              });
+            },
+            secondary: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: FlutterLogo(size: 56.0),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
