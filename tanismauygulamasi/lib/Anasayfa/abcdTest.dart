cf5f82e0d14b175ca0ae874891a2a2277071b209
Onur Aykut <onuraykut@outlook.com>
2020-04-10 21:05:16 +0300

    update

--- tanismauygulamasi/lib/Anasayfa/abcdTest.dart
+++ tanismauygulamasi/lib/Anasayfa/abcdTest.dart
@@ -9,23 +9,23 @@ class AbcdTestBolumu extends StatefulWidget {
 }
 
 class _AbcdTestBolumuState extends State<AbcdTestBolumu> {
+  double heightMedia;
+  double widthMedia;
+  List<int> siklar = new List<int>(5);
+  List<Border> border = new List<Border>(4);
+  CarouselSlider carouselSlider;
+  int soruNo=0;
   @override
   Widget build(BuildContext context) {
+    heightMedia =  MediaQuery.of(context).size.height;
+    widthMedia = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("S.A"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(gradient: GradientColors.arkaplan),
-              child: testevethayirBolumu(),
-            ),
-          ),
-        ],
+      body: Container(
+        decoration: BoxDecoration(gradient: GradientColors.arkaplan),
+        child: testevethayirBolumu(),
       ),
     );
   }
@@ -34,48 +34,14 @@ class _AbcdTestBolumuState extends State<AbcdTestBolumu> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        buttonDemo(context),
+        Container(child: buttonDemo(context)),
         Expanded(child: abcdSiklari()),
       ],
     );
   }
 
   Widget buttonDemo(context) {
-    final basicSlider = CarouselSlider.builder(
-      itemCount: 5,
-      itemBuilder: (BuildContext context, int itemIndex) => GradientCard(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-        ),
-        gradient: GradientColors.anasayfaswiper,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text("asdf"),
-              ],
-            ),
-            Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(right: 20, bottom: 20),
-                  child: Text("asdf"),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+    final basicSlider = slider();
 
     return Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -91,8 +57,8 @@ class _AbcdTestBolumuState extends State<AbcdTestBolumu> {
                       curve: Curves.linear);
                 },
                 child: Container(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: heightMedia* 0.05,
+                  width: widthMedia * 0.25,
                   padding: EdgeInsets.all(10),
                   margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
@@ -111,8 +77,8 @@ class _AbcdTestBolumuState extends State<AbcdTestBolumu> {
                       curve: Curves.linear);
                 },
                 child: Container(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: heightMedia * 0.05,
+                  width: widthMedia * 0.25,
                   padding: EdgeInsets.all(10),
                   margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
@@ -130,16 +96,101 @@ class _AbcdTestBolumuState extends State<AbcdTestBolumu> {
   Widget abcdSiklari() {
     return ListView.builder(
         itemCount: 4,
+        physics: const NeverScrollableScrollPhysics(),
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 75,
-            width: 100,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
+          return Material(
+            color: Colors.white.withOpacity(0.0),
+
+            child: InkWell(
+              onTap: () => {
+                this.setState((){
+
+                  siklar[soruNo] = index;
+
+                 for(int i=0;i<border.length;i++) {
+                   if(i!=index)
+                   border[i] = null;
+                 }
+                  border[index]= Border.all(width: 5);
+                 carouselSlider.nextPage(
+                     duration: Duration(milliseconds: 300),
+                     curve: Curves.linear);
+                 Future.delayed(const Duration(milliseconds: 500), () {
+                   setState(() {
+                     border[index] = null;
+                   });
+                 });
+
+                })
+
+              },
+              child: Container(
+                height: heightMedia*0.09,
+                margin: EdgeInsets.fromLTRB(10,5,10,5),
+                decoration: BoxDecoration(
+                  gradient: renkler[index + 12].gradient,
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: border[index],
+                ),
+                child: Center(child: Text("Bu bir şıktır")),
+              ),
             ),
           );
         });
+  }
+
+  CarouselSlider slider() {
+    carouselSlider = CarouselSlider.builder(
+      height: heightMedia*0.27,
+      itemCount: 5,
+      onPageChanged: (index) {
+        setState(() {
+          soruNo = index;
+
+          if(siklar[index]!=null) {
+            for(int i=0;i<border.length;i++) {
+              if(i!=index)
+                border[i] = null;
+            }
+            border[index]= Border.all(width: 5);
+          }
+        });
+      },
+      itemBuilder: (BuildContext context, int itemIndex) {
+       return  GradientCard(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          gradient: GradientColors.anasayfaswiper,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("Bu bir sorudur.??? Soru soru"),
+                ],
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20, bottom: 20),
+                    child: Text('${itemIndex + 1}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }
+    );
+    return carouselSlider;
   }
 }
